9e103be7c2e0f401110da6fb3e041d55060668f9
shido_lee <shido_lee@tmax.co.kr>
2020-06-04 16:25:22 +0900

    run-analyzer revised

--- analyzer/run-analyzer.sh
+++ analyzer/run-analyzer.sh
@@ -9,3 +9,11 @@ cd static_analysis
 ../scan-build cmake -DCMAKE_BUILD_TYPE=Debug ../..
 
 ../scan-build -plist-html -o ../analysis_result --use-cc /usr/lib/llvm-7/bin/clang --use-c++ /usr/lib/llvm-7/bin/clang --use-analyzer /usr/lib/llvm-7/libexec/c++-analyzer make
+
+echo 'scan-build after process start'
+#reduce html size 
+cd ../analysis_result
+echo "current dir is : $PWD" 
+find . -name "report-*.html" -exec sed -i '/class=\"codeline\"/d' {} + 
+find . -name "report-*.html" -exec sed -i '/class=\"num\"/d' {} +
+echo 'scan-build after process ends'
