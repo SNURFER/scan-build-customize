49e11629222d063ea261c539fff6717aa63327b8
shido_lee <shido_lee@tmax.co.kr>
2020-06-04 13:58:08 +0900

    scan-build copy

--- analyzer/run-analyzer.sh
+++ analyzer/run-analyzer.sh
@@ -6,6 +6,6 @@ rm -rf static_analysis
 mkdir static_analysis
 cd static_analysis
 
-scan-build cmake -DCMAKE_BUILD_TYPE=Debug ../..
+../scan-build cmake -DCMAKE_BUILD_TYPE=Debug ../..
 
-scan-build -o ../analysis_result --use-cc /usr/lib/llvm-7/bin/clang --use-c++ /usr/lib/llvm-7/bin/clang --use-analyzer /usr/lib/llvm-7/libexec/c++-analyzer make
+../scan-build -o ../analysis_result --use-cc /usr/lib/llvm-7/bin/clang --use-c++ /usr/lib/llvm-7/bin/clang --use-analyzer /usr/lib/llvm-7/libexec/c++-analyzer make
